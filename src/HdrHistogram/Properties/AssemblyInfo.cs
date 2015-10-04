5622453ce6dc951bbbc46343592d69e3cfec726b
Lee Campbell <lee.ryan.campbell@gmail.com>
2015-10-04 21:56:51 +0100

    Removed Build tasks that requried binaries to be committed to VCS

--- src/HdrHistogram/Properties/AssemblyInfo.cs
+++ src/HdrHistogram/Properties/AssemblyInfo.cs
@@ -3,6 +3,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled from attributes in the generated file AssemblyInfo.g.cs
+[assembly: AssemblyTitle("HdrHistogram")]
+[assembly: AssemblyDescription("High Dynamic Range Histogram for .NET")]
+[assembly: AssemblyCopyright("Copyright © 2015. All rights reserved.")]
 
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -11,3 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("HdrHistogram.Test")]
+
+// Versioning attributes in the generated file AssemblyInfo.g.cs
