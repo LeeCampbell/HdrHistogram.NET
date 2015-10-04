6f3e0c16a88ea00544f9fd5c54d319aa2de33596
Lee Campbell <lee.ryan.campbell@gmail.com>
2015-10-04 18:31:18 +0100

    Adding nuget package support

--- src/HdrHistogram/Properties/AssemblyInfo.cs
+++ src/HdrHistogram/Properties/AssemblyInfo.cs
@@ -2,37 +2,12 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HdrHistogram")]
-[assembly: AssemblyDescription("High Dynamic Range Histogram for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HdrHistogram.NET")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// General Information about an assembly is controlled from attributes in the generated file AssemblyInfo.g.cs
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8ef3f3a5-9d74-4b2f-8dec-5139f0d2fe9c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
 
 [assembly: InternalsVisibleTo("HdrHistogram.Test")]
