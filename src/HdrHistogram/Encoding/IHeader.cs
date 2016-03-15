742292802e5e62d45a6eca710cb1ca042e88dd52
Lee Campbell <lee.ryan.campbell@gmail.com>
2016-03-15 22:40:42 +0800

    Adding Xml Comments to IHeader

--- src/HdrHistogram/Encoding/IHeader.cs
+++ src/HdrHistogram/Encoding/IHeader.cs
@@ -1,13 +1,37 @@
 namespace HdrHistogram.Encoding
 {
+    /// <summary>
+    /// Defines the header properties to be encoded for an HdrHistogram.
+    /// </summary>
     public interface IHeader
     {
+        /// <summary>
+        /// The cookie value for the histogram.
+        /// </summary>
         int Cookie { get; }
+        /// <summary>
+        /// The length in bytes of the payload body.
+        /// </summary>
         int PayloadLengthInBytes { get; }
-        int NormalizingIndexOffset { get; }
+        /// <summary>
+        /// The normalizing index offset.
+        /// </summary>
+        int NormalizingIndexOffset { get; } //Not currently implemented/used.
+        /// <summary>
+        /// THe number of significant digits that values are measured to.
+        /// </summary>
         int NumberOfSignificantValueDigits { get; }
+        /// <summary>
+        /// The lowest trackable value for the histogram
+        /// </summary>
         long LowestTrackableUnitValue { get; }
+        /// <summary>
+        /// The highest trackable value for the histogram
+        /// </summary>
         long HighestTrackableValue { get; }
-        double IntegerToDoubleValueConversionRatio { get; }
+        /// <summary>
+        /// Integer to double conversion ratio.
+        /// </summary>
+        double IntegerToDoubleValueConversionRatio { get; } //Not currently implemented/used.
     }
 }
